bfcb8f58153be2b34486c72a8edd36d18213ee48
Gleiceara Alves de Azevedo <91923187+gleicealves@users.noreply.github.com>
2022-05-17 21:27:20 -0300

    Add files via upload

---
+++ exercicio 15.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void mostra(tfila f){
+ int l;	
+	for(l=1;l<=f.qnt;l++){
+		printf("%s:%d |", f.fila[f.inicio].nome, f.fila[f.inicio].idade);
+		if(f.inicio == MAX-1)
+		  f.inicio = 0;
+		else  
+		  f.inicio++;                 
+	}
+	
